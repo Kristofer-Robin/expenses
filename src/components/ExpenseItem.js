0151c9b6dfd8f4f6dab21d038d0ee580c28d58a6
Kristofer-Robin <kristofer-robin.lai@voco.ee>
2023-01-10 11:41:53 +0200

    Mingi asi

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,15 @@
 import './ExpenseItem.css'
+import ExpenseDate from "./ExpenseDate";
 
-function ExpenseItem (){
+function ExpenseItem (props){
+
     return(
         <div className="expense-item">
-            <div>Date</div>
+            <ExpenseDate date={props.expenseData.data}></ExpenseDate>
+            <div>{props.expenseData.date.toString()}</div>
             <div className="expense-item__description">
-                <h2>Title</h2>
-                <div className="expense-item__price">Amount</div>
+                <h2>{props.expenseData.title}</h2>
+                <div className="expense-item__price">{props.expenseData.price}$</div>
             </div>
         </div>
     )
